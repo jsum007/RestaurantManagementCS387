4dde627fc61354322ebedf1ceaaebf48f62c5475
jsum007 <63853215+jsum007@users.noreply.github.com>
2022-04-30 04:24:54 +0530

    all backend apis work

--- ResManage/ResManage/settings.py
+++ ResManage/ResManage/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'restaurant',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -47,9 +48,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+      'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'ResManage.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 TEMPLATES = [
     {
